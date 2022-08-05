907c3aad194dc260d57b3623ff71cc5db8b4e90a
Hakkı Can Akut <32385870+HakkiAkut@users.noreply.github.com>
2022-08-05 15:29:31 +0300

    my-components :: init

---
+++ my-components/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <router-view />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+};
+</script>
+
+<style>
+body {
+  background-color: #fff;
+}
+#app {
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+}
+.container {
+  width: 1240px;
+  height: 100vh;
+}
+</style>
