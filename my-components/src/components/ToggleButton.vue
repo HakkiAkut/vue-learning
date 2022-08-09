c9bb6b14c4dafd52f8305c601ac2e9237ae96414
Hakkı Can Akut <32385870+HakkiAkut@users.noreply.github.com>
2022-08-09 20:43:45 +0300

    my-components :: toggle-button created

---
+++ my-components/src/components/ToggleButton.vue
@@ -0,0 +1,94 @@
+<template>
+  <div
+    class="btn animation"
+    v-on:click="changeFilter"
+    v-bind:class="
+      interacted ? (active ? 'animation--true' : 'animation--false') : ''
+    "
+  >
+    <div class="slider"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ToggleButton",
+  data: () => ({
+    active: false,
+    interacted: false,
+    value: false,
+  }),
+  methods: {
+    changeFilter: function () {
+      this.active = !this.active;
+      this.interacted = true;
+      this.active = !this.active;
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+$height: 36px;
+$width: 60px;
+$height-slider: 22px;
+$padding: calc(($height - $height-slider) / 2);
+$movement: calc($width - $height-slider - $padding);
+$firstColor: black;
+$secondColor: white;
+
+.btn {
+  height: $height;
+  padding-left: $padding;
+  background-color: $firstColor;
+  width: $width;
+  border-radius: $height-slider;
+  display: flex;
+  align-items: center;
+  & .slider {
+    height: $height-slider;
+    background-color: $secondColor;
+    aspect-ratio: 1;
+    border-radius: 50%;
+    margin: 0;
+  }
+  &.animation {
+    animation-fill-mode: forwards;
+    animation-duration: 1s;
+    @at-root #{&}--true {
+      animation-name: changeToTrue;
+
+      & .slider {
+        background-color: $firstColor;
+      }
+    }
+    @at-root #{&}--false {
+      animation-name: changeToFalse;
+
+      & .slider {
+        background-color: $secondColor;
+      }
+    }
+  }
+  @keyframes changeToTrue {
+    0% {
+      padding-left: $padding;
+      border: 2px solid $secondColor;
+    }
+    100% {
+      padding-left: $movement;
+      background-color: $secondColor;
+      border: 2px solid $firstColor;
+    }
+  }
+  @keyframes changeToFalse {
+    0% {
+      padding-left: $movement;
+    }
+    100% {
+      padding-left: $padding;
+      background-color: $firstColor;
+    }
+  }
+}
+</style>
